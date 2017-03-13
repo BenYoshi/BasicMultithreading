ba4774c2cffb858eeaea9a5b9a268f803d965929
Benny <benyoshi42@gmail.com>
2017-03-13 15:17:14 -0400

    Update Main.java
    removed instructions comment

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,6 @@
 import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 
-/*
-    10 students wake up and go on their way to school. They wait for the guard to open the door once they get to school.
-    It takes them a random amount of time to get to school  But once there then they can wait to open the door.  All 10
-    of them must be there for the guard to open.   They will signal to him that they are there.  He will open the door,
-    they will enter, and then they will go to the classroom and again wait for all 10 of them to enter the classroom.
-    Once there a teacher will open the door and let them in to take the quiz.  Once they finish the quiz the teacher
-    will allow them to leave. Make sure that everybody in this scenario is accounted for, each thread has a name, and
-    that it either sleeps or waits.  Use a spinlock with the guard and the teacher.  Figure out a way to signal to them
-    as well.
-*/
-
 public class Main {
     public static void main(String[] args) {
         runScenario();
